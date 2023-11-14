08677fd21e01bb6965b786b77ebee4050173da13
Mathéo DELAUNAY <dseonay@gmail.com>
2023-11-14 17:56:10 +0100

    Add form for creating a folder and display image details in card

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Finder\Finder;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,11 +15,29 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+            //Ajouter un formulaire pour créér un dossier
+            $form = $this->createFormBuilder()
+            ->add('nomDuDossier', TextType::class, [
+                'label' => 'Nom du dossier',
+                'attr' => [
+                    'placeholder' => 'Nom du dossier',
+                ],
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Créer le dossier',
+                'attr' => [
+                    'class' => 'btn btn-primary',
+                ],
+            ])
+            ->getForm();
+            
+
         $finder= new Finder();
         $finder->directories()->in("img");
 
         return $this->render('home/index.html.twig', [
             "dossiers"=>$finder,
+            'formulaire' => $form->createView(),
         ]);
     }
 
