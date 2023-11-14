9c116cac732ca6c0c9339b0356a73e569c17f6b2
Mathéo DELAUNAY <dseonay@gmail.com>
2023-11-14 18:02:44 +0100

    Add form submission handling for creating a directory

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,16 +4,18 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
             //Ajouter un formulaire pour créér un dossier
             $form = $this->createFormBuilder()
@@ -31,6 +33,20 @@ class HomeController extends AbstractController
             ])
             ->getForm();
             
+            //Gestion de la soumission du formulaire
+            //1. récupérer les données du formulaire
+            $form->handleRequest($request);
+            //2. vérifier que le formulaire est soumis et valide
+            if ($form->isSubmitted() && $form->isValid()) {
+                //3. récupérer les données du formulaire
+                $data = $form->getData();
+                //4. créer le dossier
+                $fs = new Filesystem();
+                $fs->mkdir("img/".$data['nomDuDossier']);
+                //5. rediriger vers la page d'accueil
+                return $this->redirectToRoute('app_dossier', ['nomDuDossier' => $data['nomDuDossier']]);
+            }
+
 
         $finder= new Finder();
         $finder->directories()->in("img");
