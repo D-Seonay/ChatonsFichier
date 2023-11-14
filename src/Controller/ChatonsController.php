baf9e93f32ca2d5fe22478883acbfe7629038f05
Mathéo DELAUNAY <dseonay@gmail.com>
2023-11-14 11:36:26 +0100

    Add cat cards to ChatonsController index page

--- src/Controller/ChatonsController.php
+++ src/Controller/ChatonsController.php
@@ -11,8 +11,30 @@ class ChatonsController extends AbstractController
     #[Route('/', name: 'app_chatons')]
     public function index(): Response
     {
-        return $this->render('chatons/index.html.twig', [
-            'controller_name' => 'ChatonsController',
-        ]);
+        $cards = [
+            [
+                'image' => './img/cat1.jpg',
+                'altText' => 'Chat 1',
+                'title' => 'Chat 1',
+                'text' => 'Texte 1',
+                'link' => '#',
+            ],
+            [
+                'image' => './img/cat2.jpg',
+                'altText' => 'Chat 2    ',
+                'title' => 'Chat 2',
+                'text' => 'Texte 2',
+                'link' => '#',
+            ],
+            [
+                'image' => './img/cat3.jpg',
+                'altText' => 'Chat 3',
+                'title' => 'Chat 3',
+                'text' => 'Texte 3',
+                'link' => '#',
+            ],
+        ];
+    
+        return $this->render('chatons/index.html.twig', ['cards' => $cards]);
     }
 }
